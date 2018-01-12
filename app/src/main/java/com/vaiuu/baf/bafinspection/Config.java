45db6b65d4db03a4b65b71aee6ed74c583e3b4f6
ali <ali.mbstu.ict@gmail.com>
2018-01-12 12:55:47 +0600

    3nd commit

---
+++ app/src/main/java/com/vaiuu/baf/bafinspection/Config.java
@@ -0,0 +1,10 @@
+package com.vaiuu.baf.bafinspection;
+
+public class Config {
+	// Google Console APIs developer key
+	// Replace this key with your's
+	public static final String DEVELOPER_KEY = "AIzaSyCImLdt5YELNEz1LtIArq11yNtq7T-YzEs";
+	
+	// YouTube video id
+	public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
